c695a927aa56fe4fb10e4514b251b193be3be5ca
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2023-07-26 13:14:17 +0200

    Add get_member_from_user to Storage/utils.py and modify mention

--- Saturn/utils.py
+++ Saturn/utils.py
@@ -1,12 +1,25 @@
 from discord import *
 
 
-def mention(user: User) -> str:
+def mention(user_: User) -> str:
     """
     Concat user to discord's mentioning template
-    :param user:
+    :param user_:
     User to be mentioned
     :return:
     Mentioning template
     """
-    return f'<@{str(user.id)}>'
+    return f'<@{str(user_.id)}>'
+
+
+def get_member_from_user(guild_: Guild, user_: User):
+    """
+    Gets a member object of a guild from a user
+    :param guild_:
+    The `Guild` the user is in
+    :param user_:
+    The `User` is in
+    :return:
+    The user as a `Member` of said guild
+    """
+    return guild_.get_member(user_.id)
