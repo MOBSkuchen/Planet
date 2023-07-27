999799030a22394c919f0beec1d9d262db488bed
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2023-07-27 20:59:47 +0200

    Add Saturn/audio.py to Saturn/__init__.py

--- Saturn/__init__.py
+++ Saturn/__init__.py
@@ -3,3 +3,4 @@ from Saturn.translations import Translation, make_translations
 from Saturn.storage import get_bucket, Bucket
 from Saturn.goblin import Goblin
 from Saturn.utils import mention, get_vc_from_guild, get_member_from_user
+from Saturn.audio import AudioPlayer, AUDIO_PLAYERS, vc_check
