5b624bcb59ba0f194b502771e3ac6f7f12177706
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2023-12-26 19:43:58 +0100

    Reworked Goblin (Saturn/goblin.py) to use with wavelink

--- Saturn/goblin.py
+++ Saturn/goblin.py
@@ -1,121 +1,78 @@
-import pytube
-import yt_dlp
 import os
 import cv2
+import wavelink
+from discord import Embed, User, Member
 from sklearn.cluster import KMeans
 import numpy as np
 import urllib.request
-from Saturn import get_bucket
-from discord import FFmpegPCMAudio
-from urllib.request import urlopen
+from Saturn import get_bucket, get_server_translation, time_format, get_user, random_id
 
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn',
+
+ANON_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/2048px-Missing_avatar.svg.png"
+ICON_URLS = {
+    "spotify": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png",
+    "youtube": "https://yt3.googleusercontent.com/584JjRp5QMuKbyduM_2k5RlXFqHJtQ0qLIPZpwbUjMJmgzZngHcam5JMuZQxyzGMV5ljwJRl0Q=s900-c-k-c0x00ffffff-no-rj"
 }
-YTDL_OPTIONS = {
-    'format': 'bestaudio/best',
-    'extractaudio': True,
-    'audioformat': 'mp3',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',
-}
-FFMPEG = os.environ.get("FFMPEG")
 music_files_bucket = get_bucket("storage/music")
-ytdl = yt_dlp.YoutubeDL(YTDL_OPTIONS)
+DEFAULT_COLOR = 0x000000
 
 
-def wrap(url):
-    return FFmpegPCMAudio(urlopen(url), pipe=True, executable=FFMPEG)
+def get_icon_url(requester):
+    if isinstance(requester, Member):
+        return ANON_AVATAR if not requester.avatar else requester.avatar.url
+    return ICON_URLS[requester.lower()]
 
 
-class Goblin:
-    def __init__(self, pytube_obj: pytube.YouTube):
-        self.yt_obj = pytube_obj
-        self.title = self.yt_obj.title
-        self.thumbnail = self.yt_obj.thumbnail_url
-        self.author = self.yt_obj.author
-        try:
-            self.seconds = self.yt_obj.length
-        except TypeError:
-            self.seconds = 0
-        self.url = self.yt_obj.watch_url
-        self.filename = 'vid_' + self.yt_obj.video_id + ".mp3"
-        self.filename = music_files_bucket.alloc_file(self.filename)  # Pretend like file exists
-        try:
-            self.color = self.get_color()
-        except:
-            self.color = 0x000000
+def get_color(thumbnail):
+    if thumbnail is None: return DEFAULT_COLOR
+    filename = random_id() + ".jpg"
+    real_filename = music_files_bucket.alloc_file(filename)
+    urllib.request.urlretrieve(thumbnail, real_filename)
+    try:
+        img = cv2.imread(real_filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    except cv2.error:
+        print(f"ERROR [by CV2] : After operations imread, cvtColor | Returning {DEFAULT_COLOR}")
+        return DEFAULT_COLOR
+    reshape = img.reshape((img.shape[0] * img.shape[1], 3))
+    cluster = KMeans(n_clusters=5).fit(reshape)
+    centroids = cluster.cluster_centers_
+    labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
+    hist, _ = np.histogram(cluster.labels_, bins=labels)
+    hist = hist.astype("float")
+    hist /= hist.sum()
+    colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)])
+    color = int("0x" + '%02x%02x%02x' % tuple(int(c) for c in colors[len(colors) - 1][1]), base=16)
+    dc_color = color
+    music_files_bucket.Fdel(filename)
+    return dc_color
 
-    def get_color(self):
-        filename = self.filename + "_picture.jpg"
-        music_files_bucket.alloc_file(filename)
-        if not music_files_bucket.exists(os.path.basename(filename)):
-            urllib.request.urlretrieve(self.thumbnail, filename)
-        try:
-            img = cv2.imread(filename)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        except cv2.error:
-            return 0x000000
-        reshape = img.reshape((img.shape[0] * img.shape[1], 3))
-        cluster = KMeans(n_clusters=5).fit(reshape)
-        centroids = cluster.cluster_centers_
-        labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
-        hist, _ = np.histogram(cluster.labels_, bins=labels)
-        hist = hist.astype("float")
-        hist /= hist.sum()
-        colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)])
-        color = int("0x" + '%02x%02x%02x' % tuple(int(c) for c in colors[len(colors) - 1][1]), base=16)
-        dc_color = color
-        return dc_color
 
-    @staticmethod
-    def search(query: str):
-        s = pytube.Search(query)
-        try:
-            cr = s.completion_suggestions
-        except:
-            cr = []
-        return s.results, cr
+def get_embed(player: wavelink.Player, payload: wavelink.TrackStartEventPayload):
+    original: wavelink.Playable | None = payload.original
+    track: wavelink.Playable = payload.track
 
-    @staticmethod
-    def from_query(query: str, selector: int = 0):
-        r, _ = Goblin.search(query)
-        return Goblin(r[selector])
+    thumbnail = track.artwork
+    title = track.title
+    guild = player.guild
+    author = track.author
+    milliseconds = track.length
+    if recommended := original and original.recommended:
+        requester = track.source
+        requester[0].upper()
+        requester_name = requester
+    else:
+        requester = get_user(track.extras.requested_by, guild)
+        requester_name = requester.name
+    requester_icon_url = get_icon_url(requester)
 
-    @staticmethod
-    def from_url(url: str):
-        return Goblin(pytube.YouTube(url))
+    embed = Embed(color=get_color(thumbnail))
+    embed.add_field(name=f"{get_server_translation(guild, 'now_playing')}{title}",
+                      value=f"{get_server_translation(guild, 'by')}{author}\n{get_server_translation(guild, 'playing_for')}{time_format(milliseconds)}",
+                      inline=False)
+    if track.album.name is not None:
+        embed.add_field(name="Album", value=track.album.name)
+    if thumbnail is not None: embed.set_thumbnail(url=thumbnail)
+    embed.set_footer(text=f"{get_server_translation(guild, 'requested_by')}{requester_name}", icon_url=requester_icon_url)
 
-    async def get(self):
-        data = ytdl.extract_info(self.url, download=False)
-        if "entries" in data:
-            # Takes the first item from a playlist
-            data = data["entries"][0]
-        url = data["url"]
-        return wrap(url)
-
-    # Fallback
-    async def download(self):
-        if os.path.exists(self.filename):
-            return wrap(self.filename)
-        video_info = yt_dlp.YoutubeDL().extract_info(
-            url=self.url, download=False
-        )
-        options = {
-            'format': 'bestaudio/best',
-            'keepvideo': False,
-            'outtmpl': self.filename,
-        }
-        with yt_dlp.YoutubeDL(options) as ydl:
-            ydl.download([video_info['webpage_url']])
-
-        return wrap(self.filename)
+    return embed
