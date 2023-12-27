76119439e7cb22e1f2a6851d808f74eb4ab41c38
MOBSkuchen <mobskuchen@gmail.com>
2023-12-27 12:47:00 +0100

    Saturn/dc_token.py Optimized

--- Saturn/dc_token.py
+++ Saturn/dc_token.py
@@ -1,19 +1,27 @@
 import os
 
 
-def retrieve(filename, path):
+def _join(filename, path):
     if path is None:
         path = os.getcwd()
 
-    path = os.path.join(path, filename)
+    return os.path.join(path, filename)
 
+
+def retrieve(path):
     with open(path, 'r') as file:
-        return file.read()
+        content = file.read()
+    return content
 
 
 def retrieve_token(filename="token", path=None):
-    return retrieve(filename, path)
+    path = _join(filename, path)
+    return retrieve(path)
 
 
 def retrieve_debug_guilds(filename="debug_guilds", path=None):
-    return list(map(int, retrieve(filename, path).splitlines()))
+    path = _join(filename, path)
+    if not os.path.exists(path):
+        print("WARNING: No debug guilds were found!")
+        return []
+    return list(map(int, retrieve(path).splitlines()))
