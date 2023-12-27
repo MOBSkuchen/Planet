a45954971981b6b63c30077548a7f83013c0069f
MOBSkuchen <mobskuchen@gmail.com>
2023-12-27 20:59:07 +0100

    Fix bug with recommended titles embeds

--- Saturn/goblin.py
+++ Saturn/goblin.py
@@ -7,7 +7,6 @@ import numpy as np
 import urllib.request
 from Saturn import get_bucket, get_server_translation, time_format, get_user, random_id
 
-
 ANON_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Missing_avatar.svg/2048px-Missing_avatar.svg.png"
 ICON_URLS = {
     "spotify": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png",
@@ -48,14 +47,13 @@ def get_color(thumbnail):
     return dc_color
 
 
-def get_embed(player: wavelink.Player, track: wavelink.Playable):
-
+def get_embed(player: wavelink.Player, track: wavelink.Playable, recommended):
     thumbnail = track.artwork
     title = track.title
     guild = player.guild
     author = track.author
     milliseconds = track.length
-    if recommended := track.recommended:
+    if recommended:
         requester = track.source
         requester[0].upper()
         requester_name = requester
@@ -66,11 +64,12 @@ def get_embed(player: wavelink.Player, track: wavelink.Playable):
 
     embed = Embed(color=get_color(thumbnail))
     embed.add_field(name=f"{get_server_translation(guild, 'now_playing')}{title}",
-                      value=f"{get_server_translation(guild, 'by')}{author}\n{get_server_translation(guild, 'playing_for')}{time_format(milliseconds)}",
-                      inline=False)
+                    value=f"{get_server_translation(guild, 'by')}{author}\n{get_server_translation(guild, 'playing_for')}{time_format(milliseconds)}",
+                    inline=False)
     if track.album.name is not None:
         embed.add_field(name="Album", value=track.album.name)
     if thumbnail is not None: embed.set_thumbnail(url=thumbnail)
-    embed.set_footer(text=f"{get_server_translation(guild, 'requested_by')}{requester_name}", icon_url=requester_icon_url)
+    embed.set_footer(text=f"{get_server_translation(guild, 'requested_by')}{requester_name}",
+                     icon_url=requester_icon_url)
 
     return embed
