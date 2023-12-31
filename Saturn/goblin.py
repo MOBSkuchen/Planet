5bab35bf6c65f83c3d3a66c74c46acf2c7c07f14
MOBSkuchen <mobskuchen@gmail.com>
2023-12-31 22:06:54 +0100

    Update music UI color algorithm

--- Saturn/goblin.py
+++ Saturn/goblin.py
@@ -1,7 +1,6 @@
 import cv2
 import wavelink
 from discord import Embed
-from sklearn.cluster import KMeans
 import numpy as np
 import urllib.request
 from Saturn import get_bucket, get_server_translation, time_format, get_user, random_id, get_icon_url
@@ -11,29 +10,20 @@ DEFAULT_COLOR = 0x000000
 
 
 def get_color(thumbnail):
-    # This function is under maintenance and may be removed in future versions.
     if thumbnail is None: return DEFAULT_COLOR
     filename = random_id() + ".jpg"
     real_filename = music_files_bucket.alloc_file(filename)
     urllib.request.urlretrieve(thumbnail, real_filename)
-    try:
-        img = cv2.imread(real_filename)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    except cv2.error:
-        print(f"ERROR [by CV2] : After operations imread, cvtColor | Returning {DEFAULT_COLOR}")
-        return DEFAULT_COLOR
-    reshape = img.reshape((img.shape[0] * img.shape[1], 3))
-    cluster = KMeans(n_clusters=5).fit(reshape)
-    centroids = cluster.cluster_centers_
-    labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
-    hist, _ = np.histogram(cluster.labels_, bins=labels)
-    hist = hist.astype("float")
-    hist /= hist.sum()
-    colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)])
-    color = int("0x" + '%02x%02x%02x' % tuple(int(c) for c in colors[len(colors) - 1][1]), base=16)
-    dc_color = color
+    image = cv2.imread(real_filename)
+    pixels = image.reshape((-1, 3))
+    pixels = np.float32(pixels)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    _, labels, center = cv2.kmeans(pixels, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    dominant_color = center[np.argmax(np.unique(labels, return_counts=True)[1])]
+    dominant_color = int("0x" + '%02x%02x%02x' % tuple(int(c) for c in dominant_color[len(dominant_color) - 1][1]), base=16)
     music_files_bucket.Fdel(filename)
-    return dc_color
+    return dominant_color
 
 
 def get_embed(player: wavelink.Player, track: wavelink.Playable, recommended):
